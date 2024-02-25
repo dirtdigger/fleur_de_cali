825c00887f5573805ba3d61435ae4d91e32d59a4
dirtdigger <kevin.rotz@gmail.com>
2024-02-25 15:52:16 -0700

    Add files via upload
    Made model parametric.

--- fleur_de_cali_v3.py
+++ fleur_de_cali_v3.py
@@ -1,201 +1,155 @@
 import cadquery as cq
-
+import numpy as np
 from cadquery import exporters
-
 import os
 
-height    = 4 # mm
-filletRadius = 1.2
-
-# Object is created mostly as a list of rectangles
-# with centers and side lengths
-vertices = ((-52.5,6.875),
-            (-27.5,6.875),
-            ( 27.5,6.875),
-            ( 52.5,6.875),
-            (-22.5,35),
-            (22.5, 33.125),
-            (-47.5, -6.875),
-            (-22.5,-19.375),
-            (22.5, -26.15),
-            (47.5, -13.75),
-            (52.5, 6.875),
-            (0,0),
-            (47.5,18.125),
-            (6.875, 52.5), 
-            (-9.375, 47.5), 
-            (6.25, 27.5), 
-            (-21.875, 22.5), 
-            (37.5, 22.5), 
-            (0, 0), 
-            (-23.75, -22.5), 
-            (30.625, -22.5), 
-            (6.25, -27.5), 
-            (-11.25, -47.5), 
-            (18.125, -47.5), 
-            (6.875, -52.5))
-
-sideLength = ((5,36.25),
-              (5,36.25),
-              (5,36.25),
-              (5,36.25),
-              (5,30),
-              (5, 43.75),
-              (5,36.25),
-              (5,61.25),
-              (5,57.5),
-              (5,32.5),
-              (5,36.25),
-              (5,110),
-              (5,13.75),
-              (36.25, 5), 
-              (41.25, 5), 
-              (37.5, 5), 
-              (66.25, 5), 
-              (35, 5), 
-              (110, 5), 
-              (52.5, 5), 
-              (38.75, 5), 
-              (37.5, 5), 
-              (27.5, 5), 
-              (13.75, 5), 
-              (36.25, 5))
-
-N = len(vertices)
-
-# create the main frame
-result = (cq.Workplane().
-          center(vertices[0][0], vertices[0][1]).
-          rect(sideLength[0][0],sideLength[0][1]).
-          extrude(height).
-          edges("|Z").chamfer(filletRadius)
-          )
-
-for ii in range(1,N):
-    result = result + (cq.Workplane().center( vertices[ii][0], vertices[ii][1]).rect(sideLength[ii][0],sideLength[ii][1]).extrude(height).
-    edges("|Z").chamfer(filletRadius))
+# Modifiable parameters
+height    = 3 # mm, minimum is 0.8
+filletRadius = 0.8
+thickness = 4.0 #mm
+fullWidth = 150
+numMeasPoints = 5
 
 
+# The rest is all automated
+sep = fullWidth/(numMeasPoints*2)
+halfWidth = fullWidth/2
 
-# diagonals
-result2 = (cq.Workplane().
-          center(0, 47.5).
-          rect(33,5).
-          extrude(height).
-          edges("|Z").chamfer(filletRadius).
-          rotate((0,0,0),(0,0,1),-45)
-          )
-
-for ii in range(3):
-    result2 = result2.rotate((0,0,0),(0,0,1),90) + (cq.Workplane().
-              center(0, 47.5).
-              rect(33,5).
-              extrude(height).
-              edges("|Z").chamfer(filletRadius).
-              rotate((0,0,0),(0,0,1),-45)
-              )
-
-
-# results 3 and 4 are the outer diagonal pieces
-result3 = (cq.Workplane().
-          center(11.1405, 52.5).
-          rect(22.281,5).
-          extrude(height).
-          edges("|Z").chamfer(filletRadius)
-          )
-
-
-result3 = result3 + (cq.Workplane().
-          center(11.1405, -52.5).
-          rect(22.281,5).
-          extrude(height).
-          edges("|Z").chamfer(filletRadius)
-          )
-
-
-
-result3 = result3.rotate((0,0,0),(0,0,1),-45)
-
-
-result4 = (cq.Workplane().
-          center(-52.5,7.875).
-          rect(5,15.75).
-          extrude(height).
-          edges("|Z").chamfer(filletRadius)
-          )
-
-
-result4 = result4 + (cq.Workplane().
-          center(52.5, 7.875).
-          rect(5,15.75).
-          extrude(height).
-          edges("|Z").chamfer(filletRadius)
-          )
-
-result4 = result4.rotate((0,0,0),(0,0,1),-45)
-
-result = result + result2 + result3 + result4
-
-
-# Some cleanup to get rid of unsightly edges
-clean = (cq.Workplane().
-          center(57.5,0).
-          rect(5,110).
-          extrude(height).
-          edges("|Z").chamfer(filletRadius)
-          )
+def thickLine(wp, x0, x1, y0, y1, height = height, thickness = thickness):
+    dx = x1 - x0
+    dy = y1 - y0
+    
+    xbar = (x0+x1)/2
+    ybar = (y0+y1)/2
+    
+    theta = np.arctan2(dy, dx)
+    print(theta)
+    L = np.sqrt(dx**2 + dy**2)
+    
+    return (wp.center(0,0).
+                rect(L, thickness).extrude(height)
+                .edges("|Z").chamfer(filletRadius)
+                .rotate((0,0,0),(0,0,1), theta*180/np.pi)
+                .translate((xbar,ybar,0))
+                )
+    
+# Center line
+result = thickLine(cq.Workplane(), 
+                   0, 0, 
+                   -fullWidth/2 - thickness,
+                   fullWidth/2+thickness)
+# Draw all vertical pieces first
+for ii in range(numMeasPoints):
+    # Basic frame
+    result = result + thickLine(cq.Workplane(), 
+                       -(ii+1)*sep + thickness/2, 
+                       -(ii+1)*sep + thickness/2, 
+                       -fullWidth/2 - thickness,
+                        sep/2)
+    
+    result = result + thickLine(cq.Workplane(), 
+                                -(ii+1)*sep + thickness/2, 
+                                -(ii+1)*sep + thickness/2, 
+                                 sep-thickness,
+                                 fullWidth/2 + thickness)
+    # Exterior caliper stops
+    result = result + thickLine(cq.Workplane(), 
+                                -(ii+1)*sep -thickness/2, 
+                                -(ii+1)*sep -thickness/2, 
+                                -sep/2,sep)
+    
+    result = result + thickLine(cq.Workplane(),
+                                (ii+1)*sep + thickness/2, 
+                                (ii+1)*sep + thickness/2, 
+                                -sep/2,
+                                sep)
+    # Interior caliper stops
+    result = result + thickLine(cq.Workplane(), 
+                                (ii+1)*sep - thickness/2, 
+                                (ii+1)*sep -thickness/2, 
+                                -fullWidth/2 - thickness,
+                                0)
+    
+    result = result + thickLine(cq.Workplane(), 
+                                (ii+1)*sep - thickness/2, 
+                                (ii+1)*sep - thickness/2, 
+                                sep/2,
+                                fullWidth/2 + thickness)
 
 
-clean = clean + (cq.Workplane().
-          center(-57.5,0).
-          rect(5,110).
-          extrude(height).
-          edges("|Z").chamfer(filletRadius)
-          )
-
-clean = clean + (cq.Workplane().
-          center(0,57.5).
-          rect(110,5).
-          extrude(height).
-          edges("|Z").chamfer(filletRadius)
-          )
-clean = clean.rotate((0,0,0),(0,0,1),-45)
-
-clean = clean + (cq.Workplane().
-          center(-36, -52.5).
-          rect(35,5).
-          extrude(height).
-          edges("|Z").chamfer(filletRadius)
-          )
-result = result.cut(clean)
+# up til now everything has been vertical
+# rotate and make the horizontal measure points
+result = result + result.rotate((0,0,0),(0,0,1),-90)
 
 
-# Add a square center post and rotate it
-center = (cq.Workplane().
-          center(0,0).
-          rect(10,10).
-          extrude(height).
-          edges("|Z").chamfer(filletRadius)
-          )
-center = center.rotate((0,0,0),(0,0,1),-45)
+# Trim off everything that is outside of a bounding octagon
+outside = (
+    cq.Workplane()
+    .polygon(8, (fullWidth)/np.cos(np.pi/8))
+    .extrude(height).rotate((0,0,0),(0,0,1),22.5)
+)
 
-result = result + center
+osquare = (cq.Workplane()
+.polygon(4, fullWidth/np.cos(np.pi/4))
+.extrude(height))
+
+# Create the diagonals
+diag = (
+    osquare.
+    cut(
+    cq.Workplane()
+    .polygon(4, (fullWidth - 2*thickness)/np.cos(np.pi/4))
+    .extrude(height).rotate((0,0,0),(0,0,1),0)
+).intersect(outside))
+
+diag = diag.cut(thickLine(cq.Workplane(), 0,0,-fullWidth/2,fullWidth/2, thickness = sep*2) + 
+                thickLine(cq.Workplane(), -fullWidth/2,fullWidth/2, 0, 0, thickness = sep*2))
 
 
-# Chamfer the top and bottom edges to avoid
-# elephant's foot
+result = result + diag
+
+frame = (outside
+         + thickLine(cq.Workplane(), 
+                     0, 0, 
+                     -fullWidth/2 - thickness, 
+                     fullWidth/2 + thickness, 
+                     thickness = 2*(sep) - thickness)
+           .translate((thickness/2, 0, 0))
+         + thickLine(cq.Workplane(), 
+                     -fullWidth/2 - thickness, 
+                     fullWidth/2 + thickness, 
+                     0,0,
+                     thickness = 2*(sep) - thickness)
+           .translate((0, thickness/2, 0))
+         )
+result = result.intersect(frame)
+
+# diagonal caliper stops
+
+nub1 = thickLine(cq.Workplane(),
+          0, (fullWidth/2 + thickness)*np.sin(np.pi/8) + thickness,
+          fullWidth/2 + thickness/2, fullWidth/2 + thickness/2).rotate((0,0,0), (0,0,1), 45)
+
+nub1 = nub1.intersect(cq.Workplane().center(0, 0).
+                      rect(fullWidth, fullWidth).
+                      extrude(height))
+
+nub2 = nub1.mirror((1,-1,0), (0,0,0))
+
+nub3 = nub1.mirror(mirrorPlane="XZ")
+
+nub4 = nub3.mirror((1,1,0), (0,0,0))
+ 
+nub = nub1 + nub2 + nub3 + nub4
+
+result = result + nub
+
 result = result.faces("+Z").chamfer(0.4,0.4)
 
 result = result.faces("-Z").chamfer(0.4,0.4)
 
-# Finally, the top markings so you know how
-# to orient it
-bore = (cq.Workplane().
-          center(0,0).
-          circle(2).
-          extrude(3).translate((0,0,height-1))
-          )
-# Right arrow
+
+# Last things, add some flair!
 ar1 = (cq.Workplane().sketch()
         .segment((0, -0.5),
                  (8, -0.5))
@@ -204,15 +158,14 @@ ar1 = (cq.Workplane().sketch()
         .segment((8,1))
         .segment((8,0.5))
         .segment((0,0.5))
-        .close().assemble().finalize().extrude(3).translate((5,0,height-1)))
-# Left arrow
-ar2 = ar1.rotate((0,0,0),(0,0,1), 90)
-
-result = result.cut(bore + ar1 + ar2)
+        .close().assemble().finalize().extrude(height).translate((5,0,height-1)))
+ar1 = ar1 + ar1.rotate((0,0,0),(0,0,1), 90)
 
 
+result = result.cut(ar1)
 show_object(result)
-
 wd = os.getcwd()
-exporters.export(result, f'{wd}/fleur_de_cali.stl')
-exporters.export(result, f'{wd}/fleur_de_cali.step')
+exporters.export(result, f'{wd}/fleur_de_cali_{fullWidth}x{numMeasPoints}.stl')
+exporters.export(result, f'{wd}/fleur_de_cali_{fullWidth}x{numMeasPoints}.step')
+
+
