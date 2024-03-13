26eee32808ad27a85768a16c4825bb8533a72204
dirtdigger <kevin.rotz@gmail.com>
2024-03-12 18:35:32 -0700

    Some code cleanup

--- calistar.py
+++ calistar.py
@@ -5,9 +5,10 @@ import os
 
 # Modifiable parameters
 height    = 4 # mm, minimum is 0.8
-chamferSize = 0.8
-thickness = 4.0 #mm
-fullWidth = 120
+chamferSize = 0.8 # mm
+thickness = 4.0 # mm
+# Recommend fullWidth/numMeasPoints does not fall below 40
+fullWidth = 120 # mm
 numMeasPoints = 3
 diagWidth = 15
 # adjustment at the corners to account for potentially
@@ -59,7 +60,7 @@ result = thickLine(0, 0,
 # left and right lines, top and bottom
 result = result + thickLine(-sep + thickness/2, 
                             -sep + thickness/2, 
-                            sep - thickness,
+                             sep - thickness,
                             fullWidth/2)
 
 result = result + thickLine(-sep + thickness/2, 
@@ -84,19 +85,17 @@ for ii in range(numMeasPoints):
     result = result + thickLine(-(ii+1)*sep + thickness/2, 
                                 -(ii+1)*sep + thickness/2, 
                                 -sep,
-                                0)#sep/2)
+                                0)
     
     # Exterior caliper stops
     result = result + thickLine(-(ii+1)*sep -thickness/2, 
                                 -(ii+1)*sep -thickness/2, 
-#                                -sep/2,sep)
                                 -thickness,sep)
     
     
     
     result = result + thickLine((ii+1)*sep + thickness/2, 
                                 (ii+1)*sep + thickness/2, 
-                                #-sep/2, sep)
                                 -thickness,sep)
     
     result = result + thickLine((ii+1)*sep - thickness/2, 
@@ -105,10 +104,7 @@ for ii in range(numMeasPoints):
                                 0)
     
     # Interior caliper stops
-    # (thickness/2 + sep - thickness)/2
-    # = sep/2 - thickness/4
-    
-    caliperStops = caliperStops + thickLine((ii+1)*sep - thickness*2/3 - pa*2, # formerly - thickness
+    caliperStops = caliperStops + thickLine((ii+1)*sep - thickness*2/3 - pa*2,
                                 (ii+1)*sep + thickness/2,
                                 sep/2 + thickness/4,
                                 sep/2 + thickness/4)
@@ -118,20 +114,7 @@ for ii in range(numMeasPoints):
                                 sep/2-3*thickness/4,
                                 sep/2-3*thickness/4)
     
-    """
-    caliperStops = caliperStops + thickLine((ii+1)*sep - thickness/2, # formerly - thickness
-                                            (ii+1)*sep - thickness/2,
-                                            sep/2 - thickness,
-                                            sep/2,
-                                            chamferStart = False)
 
-    caliperStops = caliperStops + thickLine(-(ii+1)*sep + thickness/2, # formerly - thickness
-                                            -(ii+1)*sep + thickness/2,
-                                            sep/2,
-                                            sep/2 + thickness,
-                                            chamferEnd = False)
-    """
-    
 
 # up til now everything has been vertical
 # rotate and mirror to make the horizontal measure points
@@ -145,9 +128,6 @@ result = result + caliperStops
 # Cutouts to account for potentially poorly or non calibrated PA
 paCuts = cq.Workplane()
 for ii in range(numMeasPoints):
-    #paCuts = paCuts + (cq.Workplane().circle(pa).extrude(2*height).translate((-(ii+1)*sep - pa,-thickness/2,0)))
-    #paCuts = paCuts + (cq.Workplane().circle(pa).extrude(2*height).translate(((ii+1)*sep + pa,-thickness/2,0)))
-    
     paCuts = paCuts + (cq.Workplane().circle(pa).extrude(2*height).translate(((ii+1)*sep - pa, sep/2 - thickness/4,0)))
     paCuts = paCuts + (cq.Workplane().circle(pa).extrude(2*height).translate((-(ii+1)*sep + pa, sep/2 - thickness/4,0)))
 
@@ -177,8 +157,9 @@ for ii in range(numMeasPoints - 1):
 # Other four corners
 diag = diag + diag.mirror((0,1,0), (0,0,0))
 diag = diag + diag.rotate((0,0,0), (0,0,1), 90)
-diag = diag.rotate((0,0,0),(0,0,1),45)
 
+# rotate the whole thing and cut out the central frame
+diag = diag.rotate((0,0,0),(0,0,1),45)
 diag = diag.cut(thickLine(0,0,-fullWidth/2 - thickness,fullWidth/2 + thickness, thickness = sep*2) + 
                 thickLine(-fullWidth/2,fullWidth/2, 0, 0, thickness = sep*2))
 
@@ -189,8 +170,6 @@ center = (cq.Workplane()
                    .lineTo(0,sep/3)
                    .threePointArc((-sep/3,0),(sep/3,0))
                    .close().extrude(height))
-
-
 
 
 if fightElephantFoot:
@@ -204,6 +183,8 @@ result = result.cut((cq.Workplane()
                    .close().extrude(height).translate((0,0,height-1))))
 
 
+# If this line is causing issues, you can comment it out
+# (it's only valid if you're using CQ-editor
 show_object(result)
 wd = os.getcwd()
 exporters.export(result, f'{wd}/calistar_{fullWidth}x{numMeasPoints}.stl')
